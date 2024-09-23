87138d77d2c05652f4357bc8f9498ce0e749e614
zoyachann <zoyachanna644@gmail.com>
2024-09-23 12:10:35 +0500

    header, herosection, footer

---
+++ src/HeroSection.js
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+    return (
+        <>
+            <p>I start my journey with xWave in December 2023.
+                But in 4 months I get very information from the xWave help
+                and I will do hard work day and night. I accept in coming
+                8 months I will be a best Web Developer in my village.</p>
+        </>
+    )
+}
+export default HeroSection;
---
+++ src/header.js
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+    return (
+        <>
+            <h1>Header</h1>
+        </>
+    )
+}
+
+export default Header;
